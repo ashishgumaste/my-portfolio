45c83fced0631724dff3047da643e99551a8fabe
ashishgumaste <ashish.gumaste@gmail.com>
2018-06-26 21:17:17 -0700

    Adding script to upload zip file

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,21 @@
+import boto3
+from botocore.client import Config
+from io import BytesIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+
+portfolio_bucket = s3.Bucket('portfolio.ashishgumaste.name')
+build_bucket = s3.Bucket('portfoliobuild.ashishgumaste.name')
+
+portfolio_zip = BytesIO()
+build_bucket.download_fileobj('portfolio.zip', portfolio_zip)
+#import zipfile
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+        ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
